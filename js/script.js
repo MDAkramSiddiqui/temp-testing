ee1455a1bebb8ecb3fdf0fb8d51d921e6a89a3bd
Mohammad Akram <40524561+MDAkramSiddiqui@users.noreply.github.com>
2021-08-09 09:23:36 +0530

    added on domload event

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-// Taken from :: https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-
-function getParameterByName(name, url = window.location.href) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+function init() {
+    // Taken from :: https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    console.log("akram")
+    function getParameterByName(name, url = window.location.href) {
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+    const element = document.getElementById("div--user-id");
+    window.userId = getParameterByName('userId');
+    element.innerText = window.userId;
 }
 
-window.userId = getParameterByName('userId')
+document.addEventListener('DOMContentLoaded', () => init())
+
 
